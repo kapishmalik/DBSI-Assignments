e3068dc835ba6208013f66d8fb867b8be71ac9f4
kapishmalik <kapish15026@iiitd.ac.in>
2016-01-16 20:14:03 +0530

    - LinearHashing.java changes

--- Assignment1/src/com/assignment1/linearhashing/LinearHashing.java
+++ Assignment1/src/com/assignment1/linearhashing/LinearHashing.java
@@ -1,22 +1,28 @@
 package com.assignment1.linearhashing;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.Scanner;
 
 public class LinearHashing {
 
-	private int level              ;
-	private int nextPtr            ;
-	private int noOfBuckets        ;
-	private int noOfRecordsInserted;
-//    private String fileName        ;
+	private int level                   ;
+	private int nextPtr                 ;
+	private int totalInsertDiskAccess   ;
+	private int totalSearchDiskAccess   ;
+	private int m                       ;
+	private int noOfRecordsInserted     ;
+	private int noOfSuccessSearch       ;
 	
-    public LinearHashing()
+	//Constants
+	static private String FILENAME = "uniform.txt";
+//    private String fileName      ;
+	
+    public LinearHashing(int m)
 	{
 		this.level                = 0;
 		this.nextPtr              = 0;
-		this.noOfBuckets          = 2;
+		this.m                    = m;
 		this.noOfRecordsInserted  = 0;
 //		this.fileName             = fileName;
 	}
@@ -45,35 +51,75 @@ public class LinearHashing {
 		}
 	}
 	
-	public void insert()
-	{
-		 FileInputStream in = null;
-		 try {
-	         in = new FileInputStream("uniform.txt");
-	         int c;
-	         while ((c = in.read()) != -1) {
-	        	 //logic for insertion
-	        	 System.out.println("I am waiting for Venky to Complete. Record = "+c);
-	         }
-		 } catch (Exception e) {
-			
-		 }
-		 finally {
-	         if (in != null) {
-	            try {
-					in.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	         }
-		 }
-	}
-	public void search()
+	private void insert()
 	{
 		
+		 FileInputStream in = null;
+		 Scanner scanner;
+		 int count = 0;
+		try {
+			scanner = new Scanner(new File(FILENAME));
+			 while(scanner.hasNextLong())
+			 {
+				 long key = scanner.nextLong();
+				 System.out.println("I am waiting for Venky to Complete. Record = "+key);
+				 int mod1  = (int) (Math.pow(2,this.level)*m);
+				 int mod2  = (int) (Math.pow(2,this.level+1)*m);
+				 int hashValue = (int) (key % mod1);
+				 String diskAccessAndStatus = "|";
+				 int diskAccess=0,status=0;
+				 if((hashValue) >= this.getNextPtr())
+				 {
+					//diskAccessAndStatus = insertLh(key,hashValue);
+					 
+				 }
+				 else
+				 {
+					hashValue = (int) (key %mod2);
+				//	diskAccessAndStatus = insertLh(key,hashValue);
+					
+				 }
+				 String[] del = diskAccessAndStatus.split("|");
+				 if(del.length ==2)
+				 {
+					 diskAccess = Integer.parseInt(del[0]); 
+					 status     = Integer.parseInt(del[1]);
+				 }
+				 else
+				 {
+					 //wrong return code do something
+				 }
+				 this.totalInsertDiskAccess += diskAccess;
+				  if(status ==  -1)
+				 {
+				    // overflow Happened. Call Split function
+				     setNextPtr();
+				 }
+				
+				 count++;
+			     System.out.println("Count is "+count);
+			 }
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+	}
+	private int split()
+	{
+		//Vector<Bucket> = getBucket();
+		return 0;
+	}
+	private void search()
+	{
+	  /* 
+	     
+	*/	
 	}
 	public void simulateLinearHashing()
 	{
+		System.out.println("I am inside simulate LH");
 		insert();
 	}
 	
