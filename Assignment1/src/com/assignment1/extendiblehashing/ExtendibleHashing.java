f01d5a4e29eebb24338df7a3dc82c77a4b43812e
venky <venky10906006@gmail.com>
2016-01-23 00:01:12 +0530

    EH

---
+++ Assignment1/src/com/assignment1/extendiblehashing/ExtendibleHashing.java
@@ -0,0 +1,24 @@
+package com.assignment1.extendiblehashing;
+
+public class ExtendibleHashing {
+	
+	public ExtendibleHashing(){
+		
+	}	
+	
+	public void insert(){
+		
+	}
+	public void search(){
+		
+	}
+	
+	public void split(){
+		
+	}
+	public void simulateEH(){
+		
+	}
+	
+
+}
